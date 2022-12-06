a136a270269d6d3ffccecb4c905886909fea6e8b
Luke Farley <luke.farley@elderresearch.com>
2022-12-06 07:51:05 -0500

    Day 6 2022

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,9 +1,9 @@
-def find_first_ngram_with_distinct_chars(buffer, n):
+def find_first_group_with_distinct_chars(buffer, n):
     for i in range(len(buffer) - n):
-        ngram = buffer[i : i + n]
-        unique_chars = "".join(set(ngram))
+        group = buffer[i : i + n]
+        unique_chars = "".join(set(group))
 
-        if len(ngram) == len(unique_chars):
+        if len(group) == len(unique_chars):
             return i + n
 
 
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     with open("data/day06.txt") as f:
         buffer = f.read()
 
-    print("Part 1:", find_first_ngram_with_distinct_chars(buffer, 4))
-    print("Part 2:", find_first_ngram_with_distinct_chars(buffer, 14))
+    print("Part 1:", find_first_group_with_distinct_chars(buffer, 4))
+    print("Part 2:", find_first_group_with_distinct_chars(buffer, 14))
